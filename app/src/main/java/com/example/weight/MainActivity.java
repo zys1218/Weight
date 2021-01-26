b840e5a5c577090afeeefc2cc7a8b10347871478
zys <2662269472@qq.com>
2021-01-26 16:38:13 +0800

    initial

--- app/src/main/java/com/example/weight/MainActivity.java
+++ app/src/main/java/com/example/weight/MainActivity.java
@@ -8,11 +8,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.weight.navigation.MainNavigationActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,18 +26,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        List<String> stationList = new ArrayList<String>();
+//        List<String> nameList = new ArrayList<String>();
+//        stationList.add("请输入场站信息");
+//        nameList.add("请输入姓名");
+//        ArrayAdapter<String> stationAdapter = new ArrayAdapter<String>(this,
+//                R.layout.support_simple_spinner_dropdown_item, stationList);
+//        ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(this,
+//                R.layout.support_simple_spinner_dropdown_item, nameList);
+//
+//        Spinner statonSelection = findViewById(R.id.login_station);
+//        Spinner nameSelection = findViewById(R.id.login_name);
+//
+//        statonSelection.setAdapter(stationAdapter);
+//        nameSelection.setAdapter(nameAdapter);
+
+//        statonSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//        nameSelection.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//        });
+
         Button button = findViewById(R.id.login_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText text = findViewById(R.id.login_name);
-                String name = text.getText().toString();
-                if (TextUtils.isEmpty(name)) {
-                    Toast.makeText(MainActivity.this, "请输入姓名", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+//                Spinner nameSpinner = findViewById(R.id.login_name);
+//                String name = nameSpinner.get;
+                String name = "zys";
+//                if (TextUtils.isEmpty(name)) {
+//                    Toast.makeText(MainActivity.this, "请输入姓名", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
                 Intent intent = new Intent(MainActivity.this, MainNavigationActivity.class);
                 intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainNavigationActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/weight/householdWaste/FacilityMaintainPlanActivity.java
+++ app/src/main/java/com/example/weight/householdWaste/FacilityMaintainPlanActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.weight.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FacilityMaintainPlanActivity extends AppCompatActivity {
 
@@ -11,5 +18,33 @@ public class FacilityMaintainPlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facility_maintain_plan);
+
+        List<String> facilityList = new ArrayList<String>();
+        facilityList.add("请输入设备名称");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, facilityList);
+
+        List<String> maintainList = new ArrayList<String>();
+        facilityList.add("请输入维护内容");
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, maintainList);
+
+        Spinner facilityName = findViewById(R.id.facility_maintain_plan_facility_name);
+        Spinner maintainContent = findViewById(R.id.facility_maintain_plan_maintain_content);
+
+        facilityName.setAdapter(arrayAdapter);
+        facilityName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        maintainContent.setAdapter(arrayAdapter2);
     }
 }
