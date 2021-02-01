0625044cebe5d33acd05f0fd4b8f0a1cab33cb90
zys <2662269472@qq.com>
2021-02-01 22:08:18 +0800

    v1.1

--- app/src/main/java/com/example/weight/householdWaste/CombustiblePutrescibleActivity.java
+++ app/src/main/java/com/example/weight/householdWaste/CombustiblePutrescibleActivity.java
@@ -3,13 +3,57 @@ package com.example.weight.householdWaste;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.weight.R;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class CombustiblePutrescibleActivity extends AppCompatActivity {
+
+    private TextView comeTime, leaveTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combustible_putrescible);
+
+        comeTime = findViewById(R.id.combustible_putrescible_come_time);
+        leaveTime = findViewById(R.id.combustible_putrescible_leave_time);
+    }
+
+    public void selectComeTime(View view) {
+        Calendar calendar = Calendar.getInstance();
+        final int HOUR = calendar.get(Calendar.HOUR_OF_DAY);
+        final int MINUTE = calendar.get(Calendar.MINUTE);
+
+        new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                comeTime.setText(HOUR + ":" + MINUTE);
+            }
+        }
+        ,HOUR
+        ,MINUTE
+        ,true).show();
+    }
+
+    public void selectLeaveTime(View view) {
+        Calendar calendar = Calendar.getInstance();
+        final int HOUR = calendar.get(Calendar.HOUR_OF_DAY);
+        final int MINUTE = calendar.get(Calendar.MINUTE);
+
+        new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                leaveTime.setText(HOUR + ":" + MINUTE);
+            }
+        }
+        ,HOUR
+        ,MINUTE
+        ,true).show();
     }
 }
--- app/src/main/java/com/example/weight/householdWaste/FacilityMaintainPlanActivity.java
+++ app/src/main/java/com/example/weight/householdWaste/FacilityMaintainPlanActivity.java
@@ -19,32 +19,32 @@ public class FacilityMaintainPlanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facility_maintain_plan);
 
-        List<String> facilityList = new ArrayList<String>();
-        facilityList.add("请输入设备名称");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item, facilityList);
-
-        List<String> maintainList = new ArrayList<String>();
-        facilityList.add("请输入维护内容");
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item, maintainList);
-
-        Spinner facilityName = findViewById(R.id.facility_maintain_plan_facility_name);
-        Spinner maintainContent = findViewById(R.id.facility_maintain_plan_maintain_content);
-
-        facilityName.setAdapter(arrayAdapter);
-        facilityName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        maintainContent.setAdapter(arrayAdapter2);
+//        List<String> facilityList = new ArrayList<String>();
+//        facilityList.add("请输入设备名称");
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+//                R.layout.support_simple_spinner_dropdown_item, facilityList);
+//
+//        List<String> maintainList = new ArrayList<String>();
+//        facilityList.add("请输入维护内容");
+//        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this,
+//                R.layout.support_simple_spinner_dropdown_item, maintainList);
+//
+//        Spinner facilityName = findViewById(R.id.facility_maintain_plan_facility_name);
+//        Spinner maintainContent = findViewById(R.id.facility_maintain_plan_maintain_content);
+//
+//        facilityName.setAdapter(arrayAdapter);
+//        facilityName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//
+//        maintainContent.setAdapter(arrayAdapter2);
     }
 }
